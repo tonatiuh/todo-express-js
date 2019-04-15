4fd7f33f3095af0328c5faffc88d821d00f88f73
Tonatiuh Núñez <tonatiuh.nunez@ooma.com>
2019-04-14 19:33:43 -0500

    allow to delete; allow to toggle

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ var http = require('http');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const { Task } = require('./models');
 
 var indexRouter = require('./routes/index');
 var tasks = require('./routes/tasks');
 
 var app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,11 +16,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.locals.moment = require('moment');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use('/', indexRouter);
 app.get('/tasks', tasks.list);
 app.post('/tasks', tasks.create);
-
-app.locals.moment = require('moment');
+app.post('/tasks/toggle', tasks.toggle);
+app.post('/tasks/delete', tasks.delete);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,21 +42,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.param('task_id', async (req, _res, next, taskId) => {
-  const task = await Task.findByPk(taskId);
-  if (!task) return next(new Error('Task is not found.'));
-  req.task = task;
-
-  return next();
-});
-
 app.set('port', 3000);
-
-app.use(require('less-middleware')({
-  src: __dirname + '/public',
-  compress: true
-}));
-
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,9 @@
 const { Task } = require('./../models');
 
-exports.list = async (_req, res, _next) => {
-  const tasks = await Task.findAll();
+exports.list = async (req, res, _next) => {
+  const tasks = req.query.completed == 'true'
+    ? await Task.findAll({ order: [['id', 'DESC']], where: { completed: true }})
+    : await Task.findAll({ order: [['id', 'DESC']] })
 
   res.render('tasks', {
     title: 'Todo List',
@@ -11,12 +13,27 @@ exports.list = async (_req, res, _next) => {
 
 exports.create = async (req, res, next) => {
   if (!req.body || !req.body.name) return next(new Error('No data provided.'));
-
   const task = await Task.create({ name: req.body.name,
                                    createTime: new Date(),
                                    completed: false });
 
   if (!task) return next(new Error('Failed to save.'));
   console.info('Added %s with id=%s', task.name, task._id);
+  res.redirect('/tasks');
+}
+
+exports.toggle = async (req, res, next) => {
+  if (!req.body || !req.body.id) return next(new Error('Id is required'));
+  const task = await Task.findByPk(req.body.id);
+  if (!task) return next(new Error('Not found.'));
+
+  await task.update({ completed: !task.completed });
+  res.redirect('/tasks');
+}
+
+exports.delete = async (req, res, next) => {
+  if (!req.body || !req.body.id) return next(new Error('Id is required'));
+  const task = await Task.destroy({ where: { id: req.body.id }});
+
   res.redirect('/tasks');
 }
